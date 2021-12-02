00a2faaccdd15ac54d6c399106c56fce68f0e414
UmbertoSoffia <58592568+UmbertoSoffia@users.noreply.github.com>
2021-12-02 22:21:57 +0100

    calendario finito
    calendario finito manca solo mostrare eventi in qualche modo

--- Segfault-Android/app/src/main/java/com/example/segfault/home_user.java
+++ Segfault-Android/app/src/main/java/com/example/segfault/home_user.java
@@ -1,33 +1,54 @@
 package com.example.segfault;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.icu.util.Calendar;
-import android.media.metrics.Event;
+
+
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.EventLog;
+
+
 import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+
+import android.widget.Toast;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public class home_user extends AppCompatActivity {
 
     //https://www.tutorialspoint.com/android/android_sending_email.htm
 
-
+    private EventDay getEventDay(int day,int month,int year){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month-1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        return new EventDay(calendar, R.drawable.sample_icon);
+    }
     protected void sendEmail(String email, String msg) {
         //da provare non sono sicuro al 100% msg vada qui
         Log.i("Send email", msg);
@@ -74,8 +95,34 @@ public class home_user extends AppCompatActivity {
                 sendEmail(i.first.getMail(),"hai avuto un contatto con una persona positiva al COVID19 esattamente "+i.second+" giorni");
             }
         });
-        //il calendario non va bene quello bisogna crearlo custom da tutorial
-        //sto tutorial in ita va benisimo https://www.youtube.com/watch?v=hHjFIG0TtA0 (forse)
+        //questa e la libreria https://github.com/Applandeo/Material-Calendar-View
+        //insieme di eventi
+        List<EventDay> events = new ArrayList<>();
+
+        //aggiungere eventi al calendario
+        events.add(getEventDay(15,12,2021));
+        events.add(getEventDay(12,1,2022));
+
+
+
+
+        CalendarView calendarView =  findViewById(R.id.calendarView);
+        calendarView.setEvents(events);
+
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Calendar clickedDayCalendar = eventDay.getCalendar();
+                if(events.contains(eventDay)){
+                    Toast toast = Toast.makeText(getApplicationContext(), "devi mostrre evento", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Nessun evento per questa gionata", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
 
 
     }
