2e10c8a69858f2a50fddaf25f5887d5c3169523a
UmbertoSoffia <58592568+UmbertoSoffia@users.noreply.github.com>
2021-12-03 13:50:44 +0100

    s

--- Segfault-Android/app/src/main/java/com/example/segfault/info_utent_prom.java
+++ Segfault-Android/app/src/main/java/com/example/segfault/info_utent_prom.java
@@ -1,18 +1,63 @@
 package com.example.segfault;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Objects;
 
 public class info_utent_prom extends AppCompatActivity {
+    private LinearLayout layoutList;
+
+    //non capisco xk non va anche manifest ok e il resto del codice e uguale a dove va
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_utent_prom);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Utente promotore ");
+        layoutList = findViewById(R.id.layout_list_info_utentp);
+        String id_utent_prom=getIntent().getExtras().get("id_user").toString();
+        //come aggiungere riga alla pagina con la stringa come testo della casella
+        //il numero serve come id della struttura per poi dare valore al pulsante
+        addView("top",1);
+       addView("es",2);
+
+
+
+
+
+
+
 
     }
+    private void addView( String s,Integer id_struct) {
+
+        final View cricketerView = getLayoutInflater().inflate(R.layout.row_structure,null,false);
+
+        TextView editText = (TextView)cricketerView.findViewById(R.id.nome_struct);
+        editText.setText(s);
+/*sto bottone fa fallire non so xk
+       Button goInfo = findViewById(R.id.info_structure_user);
+       goInfo.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              /* //invio alla pagina delle info della struttura
+               Intent i = new Intent(all_structure_user.this, info_struct.class);
+               ///qua bisogna capire come trovare id della struttura per passarlo in input di la
+               i.putExtra("id_struct", id_struct.toString());
+               i.putExtra("id_user", getIntent().getExtras().get("id_user").toString());
+               startActivity(i);
+               Toast toast = Toast.makeText(getApplicationContext(), "Nessun evento per questa gionata", Toast.LENGTH_SHORT);
+               toast.show();
+           }
+       });
+*/
+
+        layoutList.addView(cricketerView);
+
+    }
+
+
 }
